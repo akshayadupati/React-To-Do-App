218ab42b45932945f267da41bcda42b9d2bc5f13
Akshaya Dupati <98553741+akshayadupati@users.noreply.github.com>
2022-09-07 23:54:08 -0400

    React To-Do Application - First commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <div className="container border border-primary rounded mt-5 bg-main">
+      <Todo />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,106 @@
+import React, { useState, useEffect } from "react";
+
+const Todo = () => {
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    if (localStorage.getItem("localTasks")) {
+      if (localStorage.getItem("localTasks") !== "undefined") {
+        const storedList = JSON.parse(localStorage.getItem("localTasks"));
+        setTasks(storedList);
+      } else {
+        setTasks([]);
+      }
+    }
+  }, []);
+
+  const addTask = (e) => {
+    if (task) {
+      const newTask = { id: new Date().getTime().toString(), title: task };
+      setTasks([...tasks, newTask]);
+      localStorage.setItem("localTasks", JSON.stringify([...tasks, newTask]));
+      setTask("");
+    }
+  };
+
+  const deleteTask = (task) => {
+    const deleted = tasks.filter((eachTask) => eachTask.id !== task.id);
+    setTasks(deleted);
+    localStorage.removeItem("localTasks");
+  };
+
+  const handleClear = () => {
+    setTasks([]);
+    localStorage.setItem("localTasks", JSON.stringify());
+  };
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+  return (
+    <div className="container row">
+      <h1 className="mt-3 mb-3 text-white text-center header-text">
+        To-Do App
+      </h1>
+      <div className="col-8">
+        <input
+          name="task"
+          type="text"
+          value={task}
+          placeholder="Add your task..."
+          className="form-control"
+          onChange={(e) => handleChange(e)}
+        />
+      </div>
+      <div className="col-4">
+        <button
+          className="btn btn-primary form-control material-icons"
+          onClick={addTask}
+        >
+          add
+        </button>
+      </div>
+      <div className="badge mt-4 mb-4 total-task">
+        You have
+        {tasks.length === 0
+          ? " no tasks"
+          : tasks.length === 1
+          ? " 1 task"
+          : ` ${tasks.length} tasks`}
+      </div>
+
+      {tasks.map((task) => (
+        <React.Fragment key={task.id}>
+          <div className="col-11">
+            <span className="form-control bg-white btn mt-2 task-item">
+              {task.title}
+            </span>
+          </div>
+          <div className="col-1 mt-1">
+            <button
+              className="mt-2 btn-warning material-icons"
+              onClick={() => deleteTask(task)}
+            >
+              delete
+            </button>
+          </div>
+        </React.Fragment>
+      ))}
+      {tasks.length > 0 ? (
+        <div className="text-center">
+          <button
+            className="btn btn-secondary mt-4 mb-4 text-uppercase text-bold bg-warning"
+            onClick={() => {
+              handleClear();
+            }}
+          >
+            Clear
+          </button>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Todo;
